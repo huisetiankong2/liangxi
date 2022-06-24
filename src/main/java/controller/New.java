1812fdd17934431cdfc46ac160c80c41ed36541a
yangda <176@da.com>
2022-06-24 20:50:35 +0800

    first

---
+++ src/main/java/controller/New.java
@@ -0,0 +1,12 @@
+package controller;
+
+
+
+
+
+public class New {
+
+    public static void main(String[] args){
+        System.out.println("1111111");
+    }
+}
---
+++ src/main/java/controller/test.java
@@ -0,0 +1,11 @@
+package controller;
+
+
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class test {
+    public static void main(String[] args) {
+
+    }
+}
