3f6adf205d1ab793c9de5e9ed71053bcc4f2455a
yangda <176@da.com>
2022-06-25 12:09:21 +0800

    pulll  sfewffwe11

--- src/main/java/controller/New.java
+++ src/main/java/controller/New.java
@@ -12,5 +12,6 @@ public class New {
         System.out.println("3333333");
         System.out.println("master");
         System.out.println("hot-fix");
+        System.out.println("hot-fix");
     }
 }
